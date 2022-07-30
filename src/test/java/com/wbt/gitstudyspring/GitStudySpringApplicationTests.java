61701ff065ecb598f3b8f053b7fb93e40a5c5367
eveningempire <3095469149@qq.com>
2022-07-30 14:19:55 +0800

    第一次提交

---
+++ src/test/java/com/wbt/gitstudyspring/GitStudySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wbt.gitstudyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitStudySpringApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
